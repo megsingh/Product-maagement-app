14b213aee767ba71defa6de3c56c11ef01dd80a0
meghna singh <megsingh2212@gmail.com>
2022-04-04 19:13:57 +0530

    updated struct tags

--- go-server/main.go
+++ go-server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
 	"go-server/router"
@@ -11,5 +10,5 @@ import (
 func main() {
 	r := router.Router()
 	fmt.Println("Starting server on the port 5000...")
-	log.Fatal(http.ListenAndServe(":5000", r))
+	http.ListenAndServe(":5000", r)
 }
--- go-server/models/models.go
+++ go-server/models/models.go
@@ -3,16 +3,16 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Product struct {
-	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Title     string             `json:"title,omitempty"`
-	Author    string             `json:"author,omitempty"`
-	Delivered bool               `json:"delivered,omitempty"`
-	User      primitive.ObjectID `json:"user,omitempty" bson:"user,omitempty"`
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Title     string             `bson:"title,omitempty"`
+	Author    string             `bson:"author,omitempty"`
+	Delivered bool               `bson:"delivered,omitempty"`
+	User      primitive.ObjectID `bson:"user,omitempty"`
 }
 
 type User struct {
-	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name     string             `json:"name,omitempty" bson:"name,omitempty"`
-	Email    string             `json:"email" gorm:"unique" validate:"required" bson:"email,required"`
-	Password string             `json:"password" validate:"required"  bson:"password,required"`
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Name     string             `bson:"name,omitempty"`
+	Email    string             `bson:"email,required"`
+	Password string             `bson:"password,required"`
 }
